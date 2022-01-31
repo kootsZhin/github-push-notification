3ab0c01a14c9eb7479edac2c46354863ad85aa24
Michael K <69758390+kootsZhin@users.noreply.github.com>
2022-02-01 03:19:15 +0800

    Update: Print updates log on console

---
+++ connectors/GithubConnector.py
@@ -0,0 +1,216 @@
+import requests
+from datetime import datetime as dt, timedelta as td
+import settings
+
+
+class GithubConnector:
+    def __init__(self, language, searchRange, pingRange):
+        self.base_url = settings.GITHUB_BASE_URL
+        self.language = language
+        self.searchRange = searchRange
+        self.pingRange = pingRange
+
+    def getLatestRepositories(self, language, searchRange, pingRange):
+        """
+        getLatestRepositories() gets the latest updates from Github API using the input query parameters
+
+        :language: target language
+        :searchRange: range while querying through Github API, min 1 day (unit: days)
+        :pingRange: range while querying through Github API, min 1 day (unit: days)
+        """
+        updates = []
+
+        timeNow = dt.utcnow()
+        timeBefore = dt.utcnow() - td(days=searchRange)
+
+        formattedTime = timeNow.strftime('%Y-%m-%d')
+        formattedTimeBefore = timeBefore.strftime('%Y-%m-%d')
+
+        queryString = f"pushed:{formattedTimeBefore}..{formattedTime} language:{language}"
+        querySort = "updated"
+        print(queryString)
+
+        queryResponse = requests.get(self.base_url + "/search/repositories", params={
+            "accept": "application/vnd.github.v3+json",
+            "q": queryString,
+            "sort": querySort,
+            "order": "desc",
+            "per_page": 100
+        }, auth=requests.auth.HTTPBasicAuth(
+            settings.GITHUB_USERNAME,
+            settings.GITHUB_ACCESS_TOKEN)
+        )
+
+        results = queryResponse.json()
+
+        for item in results["items"]:
+            itemUpdate = dt.strptime(item['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+            itemPush = dt.strptime(item['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
+
+            lastUpdate = max(itemUpdate, itemPush)
+            if lastUpdate >= timeNow - td(minutes=pingRange):
+                updates.append(item)
+
+        return updates
+
+    def getUserInfo(self, url):
+
+        try:
+            response = requests.get(url, auth=requests.auth.HTTPBasicAuth(
+                settings.GITHUB_USERNAME,
+                settings.GITHUB_ACCESS_TOKEN
+            )).json()
+
+            return {
+                "login": response["login"],
+                "id": response["id"],
+                "avatar_url": response["avatar_url"],
+                "html_url": response["html_url"],
+                "followers": response["followers"],
+                "following": response["following"],
+                "bio": response["bio"],
+                "public_repos": response["public_repos"],
+                "created_at": dt.strptime(response["created_at"], "%Y-%m-%dT%H:%M:%SZ"),
+                "updated_at": dt.strptime(response["updated_at"], "%Y-%m-%dT%H:%M:%SZ"),
+            }
+
+        except:
+            return {
+                "login": "",
+                "id": "",
+                "avatar_url": "",
+                "html_url": "",
+                "followers": "",
+                "following": "",
+                "bio": "",
+                "public_repos": "",
+                "created_at": "",
+                "updated_at": "",
+            }
+
+    def getlastCommit(self, url):
+
+        try:
+            response = requests.get(url, auth=requests.auth.HTTPBasicAuth(
+                settings.GITHUB_USERNAME,
+                settings.GITHUB_ACCESS_TOKEN)).json()
+
+            lastCommit = response[0]
+
+            return {
+                "sha": lastCommit["sha"],
+                "node_id": lastCommit["node_id"],
+                "author": lastCommit["commit"]["author"]["name"],
+                "message": lastCommit["commit"]["message"],
+            }
+
+        except:
+            return {
+                "sha": "",
+                "node_id": "",
+                "author": "",
+                "message": "",
+            }
+
+    def formatUpdates(self, updates):
+        """
+        formatUpdates() formats the filtered updates returned by Github API
+
+        :updates: list of updates returned by Github API
+        """
+        formattedUpdates = []
+
+        for item in updates:
+            tmp = {}
+            tmp["id"] = item["id"]
+            tmp["full_name"] = item["full_name"]
+            tmp["html_url"] = item["html_url"]
+            tmp["description"] = item["description"]
+
+            tmp["owner"] = self.getUserInfo(item["owner"]["url"])
+
+            tmp["created_at"] = dt.strptime(
+                item['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+            tmp["updated_at"] = dt.strptime(
+                item['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+            tmp["pushed_at"] = dt.strptime(
+                item['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
+
+            tmp["stargazers_count"] = item["stargazers_count"]
+            tmp["watchers_count"] = item["watchers_count"]
+            tmp["forks_count"] = item["forks_count"]
+            tmp["open_issues_count"] = item["open_issues_count"]
+
+            tmp["lastCommit"] = self.getlastCommit(
+                item["commits_url"][:-6])
+
+            formattedUpdates.append(tmp)
+
+        return formattedUpdates
+
+    def checkStringLength(self, string, maxLen):
+        if (string and len(string) >= maxLen):
+            return string[:maxLen] + "...(continue)"
+        else:
+            return string
+
+    def formatResponseString(self, commit):
+        resstr = ""
+
+        lastUpdate = max(commit["updated_at"], commit["pushed_at"])
+        createdAt = commit["created_at"]
+
+        timeDelta = (dt.utcnow()-lastUpdate).seconds
+        hours, remainder = divmod(timeDelta, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        name = commit["full_name"]
+        url = commit["html_url"]
+        description = self.checkStringLength(commit["description"], 180)
+
+        owner = commit["owner"]["login"]
+        followers = commit["owner"]["followers"]
+        bio = self.checkStringLength(commit["owner"]["bio"], 180)
+        publicRepos = commit["owner"]["public_repos"]
+
+        stargazers = commit["stargazers_count"]
+        watchers = commit["watchers_count"]
+        forks = commit["forks_count"]
+
+        lastCommitAuthor = commit["lastCommit"]["author"]
+        lastCommitMessage = commit["lastCommit"]["message"]
+
+        resstr += f"Last Updated: {lastUpdate} ({hours}h {minutes}m {seconds}s ago)\n"
+        resstr += f"Created At: {createdAt}\n\n"
+
+        resstr += f"Name: {name}\n"
+        resstr += f"Description: {description}\n\n"
+
+        resstr += f"Owner: {owner} (followers: {followers})\n"
+        resstr += f"Bio: {bio}\n\n"
+
+        resstr += f"Last Commit: {lastCommitMessage}\n"
+        resstr += f"Author: {lastCommitAuthor}\n\n"
+
+        resstr += f"Stargazers: {stargazers}\n"
+        resstr += f"Watchers: {watchers}\n"
+        resstr += f"Forks: {forks}\n\n"
+
+        resstr += f"URL: {url}\n"
+
+        return resstr
+
+    def print(self):
+        """
+        print() print the latest updates from Github on terminal
+        """
+        updates = self.getLatestRepositories(
+            self.language, self.searchRange, self.pingRange)
+        formattedUpdates = self.formatUpdates(updates)
+
+        for commit in formattedUpdates:
+            resstr = self.formatResponseString(commit)
+            print()
+            print("=====================================")
+            print()
+            print(resstr)
---
+++ test.py
@@ -0,0 +1,4 @@
+from connectors.GithubConnector import GithubConnector
+
+solidity = GithubConnector("Solidity", 1, 30)
+solidity.print()
